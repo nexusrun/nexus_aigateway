a89f26302999c32f1d75d42edf8906508bb64eb9
Saif Ali <saif.elyzal@gmail.com>
2026-09-15 15:11:47 -0700

    fix(api): brand Swagger docs as NEXUS AI Gateway

--- cmd/gomodel/swagger_enabled.go
+++ cmd/gomodel/swagger_enabled.go
@@ -5,5 +5,6 @@ package main
 import swaggerdocs "github.com/nexusrun/nexus_aigateway/cmd/gomodel/docs"
 
 func configureSwaggerDocs(basePath string) {
+	swaggerdocs.SwaggerInfo.Title = "NEXUS AI Gateway"
 	swaggerdocs.SwaggerInfo.BasePath = basePath
 }
